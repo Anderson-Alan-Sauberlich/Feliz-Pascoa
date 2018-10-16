00fc7faee70369e997e907373bffbb36cd4ef383
Anderson Alan Sauberlich <Anderson-Alan-Sauberlich@users.noreply.github.com>
2018-10-16 16:54:29 -0300

    Add files via upload

---
+++ Feliz_Pascoa.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <chrono>
+#include <thread>
+
+using namespace std;
+using namespace std::this_thread; // sleep_for, sleep_until
+using namespace std::chrono; // nanoseconds, system_clock, seconds
+
+int main ()
+{
+	string str("Cuuuu e linho se eu fose tu, eu tirava a mao do bolco e enfiava no ...\n");
+	
+	while (true) 
+	{
+		for (int i = 0; i < str.size(); i++){
+			cout << str[i];
+			
+			sleep_for(nanoseconds(99999999));
+		}	
+	}
+	
+	return 0;
+}
